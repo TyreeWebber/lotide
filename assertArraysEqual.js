18114e8b74f6223b5cf50e6becef22b668d362c8
Tyree Webber <webber.tyree@gmail.com>
2022-06-06 17:38:25 -0600

    refactored to remove test statements and exported them to a test folder/file.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,5 @@
-//eqArrays function
-const eqArrays = function(actual, expected) {
-  let match = true;
-  if (actual.length !== expected.length) {
-    match = false;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      match = false;
-    }
-  }
-  return match;
-};
-
 //assertArrayEqual 
+const eqArrays = require('./eqArrays');
 const assertArraysEqual = function( actual, expected) {
   if (eqArrays(expected, actual)) {
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected} `)
@@ -20,6 +7,4 @@ const assertArraysEqual = function( actual, expected) {
     console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected} `)
   }
 };
-
-assertArraysEqual(["stickbug", "stinkbug", "bugbug"], ["stickbug", "stinkbug", "bugbug"]);
-assertArraysEqual(["stickbug", "stinkbug", "bugbug"], ["stickbug", "stinkbug", "bug"]);
+module.exports = assertArraysEqual;
