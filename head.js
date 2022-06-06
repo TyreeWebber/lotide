88d96d72f99c15a027773012210b982ae05e9730
Tyree Webber <webber.tyree@gmail.com>
2022-06-06 16:53:13 -0600

    refactored to remove test statements and exported to a test folder/file.

--- head.js
+++ head.js
@@ -1,17 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected} `);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected} `);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(actual) {
   return actual[0];
 };
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Stinky"]));
-assertEqual(head[""]);
+module.exports = head;
