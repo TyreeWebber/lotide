a97d9e7b62042005c62b60dced48291f3ffa8dcb
Tyree Webber <webber.tyree@gmail.com>
2022-06-06 17:19:44 -0600

    refactored to remove test statements and exported them to a test folder/file.

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,8 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected} `);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected} `);
-  }
-};
-
 //create function that takes in 2 arrays
 //return true or false based on perfect match
 //if array1 !== array 2 assertion should fail
 //if values within array1 & array2 aren't the same assertion should fail
 //EDGECASE: if the function does not return true for nested arrays or arrays of objects that are identical
-
 const eqArrays = function(actual, expected) {
   let match = true;
   if (actual.length !== expected.length) {
@@ -24,11 +15,4 @@ const eqArrays = function(actual, expected) {
   }
   return match;
 };
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
-assertEqual(eqArrays(["Stickbug", "Stinkbug", "Bugbug"], ["Stickbug", "Stinkbug", "Bigbug"]), true);
+module.exports = eqArrays;
