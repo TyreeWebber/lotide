c126a4b24ec8ff21a6303bd503d6bf45dba7b6a1
Tyree Webber <webber.tyree@gmail.com>
2022-06-06 19:39:39 -0600

    refactored test to use mocha & chai.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,8 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
 const middle = require('../middle');
-//Test
-assertArraysEqual(middle([1, 2, 3]), [2]);
+
+describe("#middle", () => {
+  it("it returns the middle value of an array if there is one", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+}); 
